7ad6b9fd4090aa92f823a2637723aeeca0b7207e
Jackson Ling <jackson.llk@gmail.com>
2022-07-01 10:36:44 +0800

    replaced block dict with block object

--- blockchain.py
+++ blockchain.py
@@ -49,13 +49,7 @@ def load_data():
     except IOError:
         print("File Not Found!")
         # Our starting blockchain
-        genesis_block = {
-            'previous_hash': '',
-            'index': 0,
-            'transactions': [],
-            'proof': 100
-        }
-
+        genesis_block = Block(0, '', [], 100, 0)
         # Initializing our empty blockchain list
         blockchain = [genesis_block]
         # Unhandled transactions
@@ -116,7 +110,7 @@ def proof_of_work():
 
 
 def get_balance(participant):
-    tx_sender = [[tx['amount'] for tx in block['transactions']
+    tx_sender = [[tx['amount'] for tx in block.transactions
                   if tx['sender'] == participant] for block in blockchain]
     open_tx_sender = [tx['amount']
                       for tx in open_transactions if tx['sender'] == participant]
@@ -125,7 +119,7 @@ def get_balance(participant):
     amount_sent = reduce(lambda tx_sum, tx_amt: tx_sum +
                          tx_amt[0] if len(tx_amt) > 0 else 0, tx_sender, 0)
 
-    tx_recipient = [[tx['amount'] for tx in block['transactions']
+    tx_recipient = [[tx['amount'] for tx in block.transactions
                      if tx['recipient'] == participant] for block in blockchain]
     amount_received = reduce(lambda tx_sum, tx_amt: tx_sum +
                              tx_amt[0] if len(tx_amt) > 0 else 0, tx_recipient, 0)
@@ -182,12 +176,7 @@ def mine_block():
         [('sender', 'MINING'), ('recipient', owner), ('amount', MINING_REWARD)])
     copied_transactions = open_transactions[:]
     copied_transactions.append(reward_transaction)
-    block = {
-        'previous_hash': hashed_block,
-        'index': len(blockchain),
-        'transactions': copied_transactions,
-        'proof': proof
-    }
+    block = Block(len(blockchain), hashed_block, copied_transactions, proof)
     blockchain.append(block)
     return True
 
@@ -219,9 +208,9 @@ def verify_chain():
     for (index, block) in enumerate(blockchain):
         if index == 0:
             continue
-        if block['previous_hash'] != hash_block(blockchain[index - 1]):
+        if block.previous_hash != hash_block(blockchain[index - 1]):
             return False
-        if not valid_proof(block['transactions'][:-1], block['previous_hash'], block['proof']):
+        if not valid_proof(block.transactions[:-1], block.previous_hash, block.proof):
             print('Proof of work is invalid')
             return False
     return True
@@ -240,7 +229,6 @@ while waiting_for_input:
     print('3: Output a blockchain blocks')
     print('4: Output participants')
     print('5: Check transaction validity')
-    print('h: Manipulate the chain')
     print('q: Quit')
     user_choice = get_user_choice()
     if user_choice == '1':
@@ -265,14 +253,6 @@ while waiting_for_input:
             print('All transactions are valid')
         else:
             print('There are invalid transactions')
-    elif user_choice == 'h':
-        # Make sure that you don't try to hack the blockchain if it's empty
-        if len(blockchain) >= 1:
-            blockchain[0] = {
-                'previous_hash': '',
-                'index': 0,
-                'transactions': [{'sender': 'John', 'recipient': 'Jackson', 'amount': 100.0}]
-            }
     elif user_choice == 'q':
         # This will lead to the loop to exit because it's running condition
         waiting_for_input = False

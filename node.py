16fbd46ad42d15bf71af842c566698bad33f81e2
Jackson Ling <jackson.llk@gmail.com>
2022-08-01 20:10:48 +0800

    added GET route to fetch a list of all nodes

--- node.py
+++ node.py
@@ -177,7 +177,7 @@ def add_node():
     return jsonify(response), 201
 
 
-@app.route('/node/<node_url>', methods =['DELETE'])
+@app.route('/node/<node_url>', methods=['DELETE'])
 def remove_node(node_url):
     if node_url == '' or node_url == None:
         response = {
@@ -190,6 +190,16 @@ def remove_node(node_url):
         'all_nodes': blockchain.get_peer_nodes()
     }
     return jsonify(response), 200
+
+
+@app.route('/nodes', methods=['GET'])
+def get_nodes():
+    nodes = blockchain.get_peer_nodes()
+    response = {
+        'all_nodes': nodes
+    }
+    return jsonify(response), 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
